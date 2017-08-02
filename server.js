f8f1828b3144e4050c0c7a4a67623eee175c76b1
System <none>
2017-08-02 23:55:54 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // init project
 var path = require('path');
 const bodyParser = require('body-parser');
+const os = require('os');
 var express = require('express');
 var app = express();
 
@@ -8,9 +9,15 @@ var app = express();
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// routes
+app.get('/api/whoami', (req, res) => {
+  
+  return {
+    "ipaddress": req.ip,
+    "language": ,
+    "software": "Macintosh; Intel Mac OS X 10_12_6"}
+  }
+});
 
 
 // listen for requests :)
